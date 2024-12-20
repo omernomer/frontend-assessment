b676748e188a7b74858caa0286795322695a45a7
Omar Al-Debagy <omeraldebagy@gmail.com>
2024-12-20 18:07:30 +0100

    created TableHead component

---
+++ src/components/TableHead/TableHead.tsx
@@ -0,0 +1,42 @@
+import { colors, greyThinBorder } from "../../styleConstants";
+import { SortOptions, StylingObject } from "../../types";
+
+const styles: StylingObject = {
+  th: {
+    cursor: "pointer",
+    textTransform: "capitalize",
+    padding: "5px",
+    border: greyThinBorder,
+    minWidth: "100px",
+    position: "sticky",
+    top: "0",
+    background: colors.white,
+  },
+};
+
+interface TableHeadProps {
+  sortCountriesBy: (key: SortOptions) => void;
+  renderSortIcon: (key: SortOptions) => string;
+}
+
+function TableHead({ sortCountriesBy, renderSortIcon }: TableHeadProps) {
+  return (
+    <thead>
+      <tr>
+        {Object.values(SortOptions).map((key) => {
+          return (
+            <th
+              style={styles.th}
+              key={key}
+              onClick={() => sortCountriesBy(key)}
+            >
+              {key} {renderSortIcon(key)}
+            </th>
+          );
+        })}
+      </tr>
+    </thead>
+  );
+}
+
+export default TableHead;
