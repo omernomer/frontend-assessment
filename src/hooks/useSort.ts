469145e62f42850f855ba01c93c46f29ee49790a
Omar Al-Debagy <omeraldebagy@gmail.com>
2024-12-20 18:08:40 +0100

    created hooks

---
+++ src/hooks/useSort.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { SortDirection, SortOptions } from "../types";
+
+export const useSort = (
+  initialSortBy: SortOptions,
+  initialSortDirection: SortDirection
+) => {
+  const [sortBy, setSortBy] = useState<SortOptions>(initialSortBy);
+  const [sortDirection, setSortDirection] = useState<SortDirection>(initialSortDirection);
+  const sortCountriesBy = (key: SortOptions) => {
+    if (sortBy === key) {
+      setSortDirection(
+        sortDirection === SortDirection.asc ? SortDirection.desc : SortDirection.asc
+      );
+    } else {
+      setSortBy(key);
+      setSortDirection(SortDirection.asc);
+    }
+    // Scroll to the top of the page
+    document.body.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const isCurrentSort = (key: SortOptions) => {
+    return sortBy === key;
+  };
+
+  const renderSortIcon = (key: SortOptions) => {
+    if (isCurrentSort(key)) {
+      return sortDirection === SortDirection.asc ? "🔼" : "🔽";
+    }
+    return "";
+  };
+
+  return { sortBy, sortDirection, sortCountriesBy, renderSortIcon };
+};
---
+++ src/hooks/useSortedCountries.ts
@@ -0,0 +1,18 @@
+import { useMemo } from "react";
+import countriesList from "../data/countries";
+import { SortDirection, SortOptions } from "../types";
+
+export const useSortedCountries = (
+  sortBy: SortOptions,
+  sortDirection: SortDirection
+) => {
+  return useMemo(() => {
+    return [...countriesList].sort((a, b) => {
+      if (sortDirection === SortDirection.asc) {
+        return a[sortBy] > b[sortBy] ? 1 : -1;
+      } else {
+        return a[sortBy] < b[sortBy] ? 1 : -1;
+      }
+    });
+  }, [countriesList, sortBy, sortDirection]);
+};
