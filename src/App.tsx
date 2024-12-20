83663b3603b154a70ed0514cf3cffd580a6bd561
Omar Al-Debagy <omeraldebagy@gmail.com>
2024-12-20 18:14:37 +0100

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import TableBody from "./components/TableBody";
+import TableHead from "./components/TableHead";
+import { useSort } from "./hooks/useSort";
+import { useSortedCountries } from "./hooks/useSortedCountries";
+import { SortDirection, SortOptions, StylingObject } from "./types";
+
+const styles: StylingObject = {
+  table: {
+    borderCollapse: "collapse",
+    width: "100%",
+  },
+};
 
 function App() {
-  return <table></table>;
+  const { sortBy, sortDirection, sortCountriesBy, renderSortIcon } = useSort(
+    SortOptions.country,
+    SortDirection.asc
+  );
+  const sortedCountries = useSortedCountries(sortBy, sortDirection);
+
+  return (
+    <table style={styles.table}>
+      <TableHead
+        sortCountriesBy={sortCountriesBy}
+        renderSortIcon={renderSortIcon}
+      />
+      <TableBody sortedCountries={sortedCountries} />
+    </table>
+  );
 }
 
 export default App;
