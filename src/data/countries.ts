0cf995dfbe970fed4fba870ef8f4969dd7858bb3
Omar Al-Debagy <omeraldebagy@gmail.com>
2024-12-20 18:05:55 +0100

    moved and refactored countries.ts

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,19 +1,4 @@
-interface Country {
-  confirmed: number;
-  recovered: number;
-  deaths: number;
-  country: string;
-  population: number;
-  areaInSqKm: number;
-  lifeExpectancy: number;
-  elevationMeters: number;
-  continent: string;
-  abbreviation: string;
-  location: string;
-  capitalCity: string;
-  lat: number;
-  lng: number;
-}
+import { Country } from "../types";
 
 const countries: Country[] = [
   {
